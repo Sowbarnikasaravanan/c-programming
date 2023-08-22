606e9ee9ad6486614856649f7e90a99d91def175
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-08-22 19:43:09 +0530

    Multiplication of floating numbers

---
+++ Multiply.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+  float a,b,mul;
+  printf("Enter two numbers:");
+  scanf("%f %f",&a,&b);
+  mul=a*b;
+  printf("The multiplication of %f and %f is %f",a,b,mul);
+}
