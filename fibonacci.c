e95dd9912a8b5f7694026bcd51d29e6328ef6332
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-08-30 17:20:00 +0530

    fibanocci of a number

--- fibonacci.c
+++ fibonacci.c
@@ -1,5 +1,25 @@
 // To display fibonacci sequence
 #include<stdio.h>
 void main(){
-  int number,fib;
-  printf("
+  int n,fib;
+  printf("Enter the number of terms:"); 
+  scanf("%d",&n);
+  int t1=0,t2=1,i;
+  if(n==1)
+    printf("Fibonacci series of first term is %d",n,t1);
+  else if(n==2)
+      printf("Fibonacci series of %d are %d,%d",n,t1,t2);
+  else if(n>2){
+      int next=t1+t2;
+      printf("Fibonacci series of %d numbers are : %d %d ",n,t1,t2); 
+      for(i=3;i<=n;i++){
+          printf(" %d ",next);
+          t1=t2;
+          t2=next;
+          next=t1+t2;
+      }
+  }
+  else{
+      printf("Sorry! You entered the negative value");
+  }
+}
