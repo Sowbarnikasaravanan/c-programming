784cc0b4816c2ecfa370f3e8762ca4d7d2379df3
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-08-30 22:57:40 +0530

    counting the number of digits of a number

--- countdigits.c
+++ countdigits.c
@@ -1,12 +1,11 @@
 //To count a number of digits of an integer
 #include<stdio.h>
 void main(){
-  int number,count,rem,temp;
+  int number,count=0,temp;
   printf("Enter a number:");
   scanf("%d",&number);
   temp=number;
   while(number!=0){
-      rem=number%10;
       number=number/10;
       count++;
   }
