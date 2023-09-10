6920ebe6b1c03a78c481e21e0d51b441673c2df6
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-09-10 19:20:20 +0530

    gcd using recursion

---
+++ gcdusingrecursion.c
@@ -0,0 +1,24 @@
+//To find the gcd using recursion
+#include<stdio.h>
+int gcd(int,int);
+void main(){
+  int num1,num2;
+  printf("Enter two numbers:");
+  scanf("%d %d",&num1,&num2);
+  printf("The gcd of  %d and %d is %d",num1,num2,gcd(num1,num2));
+}
+int gcd(int a,int b){
+    int t;
+    if(a<b){
+        t=a;
+        a=b;
+        b=t;
+    }
+    
+    if(b!=0){
+        return gcd(b,a%b);
+    }
+    else{
+        return a;
+    }
+}
