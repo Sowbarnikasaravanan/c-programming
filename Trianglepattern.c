b7bac780841705285c61717e9a5f5ecf8863a600
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-12 13:43:26 +0530

    To display the triangle pattern

---
+++ Trianglepattern.c
@@ -0,0 +1,14 @@
+// To display the numbers in triangle pattern 
+#include<stdio.h>
+void main(){
+    int number,i,j;
+    printf("Enter the no. of rows:");
+    scanf("%d",&number);
+    rows=int(input("Enter the numner of rows :"))
+    for(i=0;i<n;i++){
+       for(j=0;j<n;j++){
+            printf("* ");
+       }
+       printf("\n");
+    }
+}
