4e68ede1fbadeda33f09fa2cb5f1f0dbe1cbd49d
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-17 19:54:50 +0530

    Program for right of the pyramid

---
+++ Righthalfpyramid.c
@@ -0,0 +1,15 @@
+//To display the right half pyramid 
+void main(){
+    int i,j,k,num;
+    printf("Enter the number of rows:");
+    scanf("%d",&num);
+    for(i=0;i<num;i++){
+       for(j=0;j<2*(num-i)-1;j++){
+           printf(" ");
+       }
+       for(k=0;k<=i;k++){
+           printf(" *");
+       }
+       printf("\n");
+    }
+}
