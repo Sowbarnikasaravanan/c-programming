3a60d2a82816b20908014a65404f35a652ffd7f2
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-11-07 23:40:04 +0530

    To display the inverted right triangle

---
+++ Invertedrightriangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+    int n,i,j,k;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+       for(j=0;j<2*(n-i)-1;j++){
+           printf(" ");
+       }
+       for(k=0;k<=i;k++){
+           printf("* ");
+       }
+        printf("\n");
+    }
+}
+
+
