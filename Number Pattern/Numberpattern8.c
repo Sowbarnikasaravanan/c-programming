47fd1dd4459922133ce71a67ab9efb63fd8bb9aa
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-11-16 23:06:56 +0530

    numberpattern 8

---
+++ Number Pattern/Numberpattern8.c
@@ -0,0 +1,19 @@
+/*To display the numbers in right angled triangle
+1
+22
+333
+4444
+55555
+*/
+#include<stdio.h>
+void main(){
+  int n,i,j;
+  printf("Enter the number:");
+  scanf("%d",&n);
+  for(i=i;i<=n;i++){
+    for(j=1;j<=i;j++){
+      printf("%d",i);
+    }
+    printf("\n");
+  }
+}
