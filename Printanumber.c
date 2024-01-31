bf75a64a8ce318f534f6020987b168d1a06d93ff
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-31 21:51:17 +0530

    Print a number entered by an user

---
+++ Printanumber.c
@@ -0,0 +1,13 @@
+// To print a number that is entered by an user 
+#include<stdio.h>
+void main(){
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    printf("You entered:%d",n);
+}
+
+/* 
+Enter the number:5
+You entered:5
+*/
