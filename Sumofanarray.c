420dd0a9dd2e77416481d397e278ac519d228f3f
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-02-07 19:47:36 +0530

    To find the sum of an array

---
+++ Sumofanarray.c
@@ -0,0 +1,16 @@
+// To sum the elements of an array that are characters
+#include <stdio.h>
+int main() {
+    char s[]={1,2,3,4,5};
+    int i,ele,sum=0;
+    for(i=0;i<n;i++){
+        ele=(int)s[i];
+        sum+=ele;
+    }
+    printf("%d",sum);
+}
+
+/*
+Sample Output:
+15
+*/
