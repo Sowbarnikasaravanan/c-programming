610bab111b0b6e979aa8ca69c28a25305ff5bb94
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-03-02 19:44:01 +0530

    Starpattern 03

---
+++ StarPattern/Starpattern03.c
@@ -0,0 +1,19 @@
+/* To display the star pattern 
+*****
+****
+***
+**
+*
+*/
+#include<stdio.h>
+void main(){
+    int n,i,j;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
