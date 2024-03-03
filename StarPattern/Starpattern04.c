154fec1e373e953e86516e9afc2845e0e4b9aaf2
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-03-03 21:01:50 +0530

    Star pattern 04

--- StarPattern/Starpattern04.c
+++ StarPattern/Starpattern04.c
@@ -1,1 +1,24 @@
-/
+/* To display the star pattern 
+     *
+    **
+   ***
+  ****
+ *****
+******
+*/
+#include<stdio.h>
+void main(){
+    int n,i,j,k;
+    printf("Entevoidr the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(k=1;k<=i;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
