66355752f892d2daa68bb4a98ffc7211174e3744
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-03-20 21:13:47 +0530

    To reverse a string

--- Reverseastring.c
+++ Reverseastring.c
@@ -2,12 +2,21 @@
 #include<stdio.h>
 #include<string.h>
 void main(){
-  char a[30],rev[30],i,l,k=0;
-  printf("Enter the string:");
-  scanf("%s",a);
-  l=strlen(a);
-  for(i=l-1;i>=0;i--){
-      rev[k++]=a[i];
-  }
-  printf("%s",rev);
+    char a[30],temp;
+    int i,l;
+    printf("Enter the string:");
+    scanf("%s",a);
+    l=strlen(a);
+    for(i=0;i<l/2;i++){
+        temp=a[i];
+        a[i]=a[l-i-1];
+        a[l-i-1]=temp;
+    }
+    printf("%s",a);
 }
+
+/*
+Sample output:
+Enter the string:sowbarnika
+akinrabwos
+*/
