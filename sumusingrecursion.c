f5ca5098ea16c652b254023d5722a8e47516fc6f
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-04-08 22:42:24 +0530

    To find sum of natural numbers using recursion

--- sumusingrecursion.c
+++ sumusingrecursion.c
@@ -1,6 +1,14 @@
 //To find the sum of n natural nummbers using recursion
 #include<stdio.h>
-int sum(int );
+int sum(int n){
+    if(n==0){
+      return 0;
+    }
+    else{
+      return n+sum(n-1);
+    }
+   
+}
 void main(){
   int n,s;
   printf("Enter the n of natural number:");
@@ -14,7 +22,6 @@ int sum(int n){
     }
     else{
       return n+sum(n-1);
-    }
-   
+    }  
 }
       
