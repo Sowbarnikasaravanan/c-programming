cec4e07d0469c6afd4df3643668b3a479d6ea0e3
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-05-14 22:00:46 +0530

    To find minimum common elements

---
+++ MinimumCommonElements.c
@@ -0,0 +1,32 @@
+// To find the minimum common element of two arrays
+#include<stdio.h>
+void main(){
+    int a[20],b[20],i,j,n,m,min=0;
+    printf("Enter the number of elements in first array:");
+    scanf("%d",&m);
+    printf("Enter the number of elements in second array:");
+    scanf("%d",&n);
+    printf("Enter the elements of array 1:");
+    for(i=0;i<m;i++){
+         scanf("%d",&a[i]);
+    }
+    printf("Enter the elements of array 2:");
+    for(j=0;j<n;j++){
+        scanf("%d",&b[j]);
+    }
+    for(i=0;i<m;i++){
+        for(j=i+1;j<m;j++){
+            if(a[i]>=a[j]){
+                
+            }
+        }
+    }
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            if(a[i]==a[j]){
+               min=a[i];
+            }
+        }
+    }
+    printf("Minimum common element between two arrays are:%d",min);
+}
